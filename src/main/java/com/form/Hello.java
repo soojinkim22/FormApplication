f030daa26f1ecdeb59ef33be00458aace394b553
Soojin Kim <soojinkim22@gmail.com>
2016-06-10 17:07:44 +0900

    init
    setting

---
+++ src/main/java/com/form/Hello.java
@@ -0,0 +1,22 @@
+package com.form;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Hello {
+	@RequestMapping("/")
+	public String index(Model model){
+		model.addAttribute("login", "Success Login page");
+		
+		return "index";
+	}
+	
+	@RequestMapping("/hello")
+	public String hello(Model model){
+		model.addAttribute("message", "sample");
+		
+		return "hello";
+	}
+}
