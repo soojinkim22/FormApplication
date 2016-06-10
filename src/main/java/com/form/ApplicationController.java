3911ca7e8f614ead5877485ceb1e3f328b9db1b7
Soojin Kim <soojinkim22@gmail.com>
2016-06-10 17:48:24 +0900

    controller 名前変更
    ...

--- src/main/java/com/form/ApplicationController.java
+++ src/main/java/com/form/ApplicationController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class Hello {
+public class ApplicationController {
 	@RequestMapping("/")
 	public String index(Model model){
 		model.addAttribute("login", "Success Login page");
