5d907523ea5e7c67335fbf5e5cf482bccbf1aca5
ann7o <anna.shinozaki@casleyconsulting.co.jp>
2016-06-13 14:55:20 +0900

    choices dao

--- src/main/java/com/form/dao/ChoicesDAO.java
+++ src/main/java/com/form/dao/ChoicesDAO.java
@@ -1,5 +1,22 @@
 package com.form.dao;
 
-public class ChoicesDAO {
+import java.util.ArrayList;
 
+import javax.sql.DataSource;
+
+
+public class ChoicesDAO{
+
+	public void setDataSource(DataSource dataSource) {
+	      this.dataSource = dataSource;
+	      this.jdbcTemplateObject = new JdbcTemplate(dataSource);
+	}
+
+	public ArrayList<choicesDAO> selectByAll() {
+		String SQL = "select * from choices_tb";
+
+		ArrayList<choicesDAO> choicesDAO = jdbcTemplateObject.query(SQL.new choicesMapper());
+
+		return choicesDAO;
+	}
 }
